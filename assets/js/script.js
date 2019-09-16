d3bfc306c56360227ea0de88337b10a021d8fc39
akimestr <mkestrade@gmail.com>
2019-09-15 23:50:41 -0300

    Upload do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+/* EVENTO SCROLL -> MUDAR O MENU
+
+jQuery(function () {
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > 400) {
+     $("#menu").addClass("menu-diferente");
+    } else {
+     $("#menu").removeClass("menu-diferente");
+    }
+  });
+}); */
+
+$(window).bind('scroll', function () {
+      if ($(window).scrollTop() > 350) {
+        $(".menu_mluv_retratil").addClass('mostrar');
+      } else if ($(window).scrollTop() < 350) {
+        $(".menu_mluv_retratil").removeClass('mostrar');
+      }
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  ga('send', 'event', 'galeria', 'next_prev', 'Titulo da página');
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
